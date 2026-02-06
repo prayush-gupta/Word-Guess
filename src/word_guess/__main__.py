76b54a11855824ee6f4d0e415b589a1662fc41ba
prayush-gupta <prayushgupta121293@gmail.com>
2026-02-06 14:31:56 +0400

    FInally, it works!!

--- src/word_guess/__main__.py
+++ src/word_guess/__main__.py
@@ -1,11 +1,11 @@
 """Run the game when the package is executed with python -m word_guess."""
 
 from word_guess import game_start
-from scripts.raw_data_upload_to_db import data_loader
+# from scripts.raw_data_upload_to_db import data_loader
 
 
 def main() -> None:
-    data_loader()
+    # data_loader()
     game_start()
 
 if __name__ == "__main__":
